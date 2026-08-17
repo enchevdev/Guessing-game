925f02e4e1f87e209770677eb421c2d44d600226
Serkan14520 <enchevserkan@gmail.com>
2026-08-17 14:46:41 +0200

    Add random secret number and player input

--- Program.cs
+++ Program.cs
@@ -1,10 +1,23 @@
-﻿namespace Guessing_game
+﻿using static System.Runtime.InteropServices.JavaScript.JSType;
+
+namespace Guessing_game
 {
     internal class Program
     {
         static void Main(string[] args)
         {
-            Console.WriteLine("Hello, World!");
+
+            int gok = 0;
+
+
+
+            Random number = new Random();
+
+            int geheimGetal =
+
+            number.Next(1, 101);
+            Console.WriteLine("Raad het geheime getal tussen 1 en 100");
+            gok = int.Parse(Console.ReadLine());
         }
     }
 }
