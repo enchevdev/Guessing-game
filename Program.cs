b76786c09423648f0c40c269a96408c0fed7327c
Serkan14520 <enchevserkan@gmail.com>
2026-08-17 14:14:23 +0200

    Initial commit

---
+++ Program.cs
@@ -0,0 +1,10 @@
+﻿namespace Guessing_game
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Hello, World!");
+        }
+    }
+}
