34084dc499fcf237843aaa2512bb71c61b624155
Serkan14520 <enchevserkan@gmail.com>
2026-08-19 21:28:02 +0200

    Validate guesses and count valid attempts

--- Program.cs
+++ Program.cs
@@ -17,11 +17,11 @@
             while (gok != geheimGetal)
             {
                 Console.WriteLine("Raad het geheime getal tussen 1 en 100");
-                
+
                 try
                 {
                     gok = int.Parse(Console.ReadLine());
-                    gokTeller++;
+                    
                 }
 
                 catch (FormatException)
@@ -29,24 +29,28 @@
                     Console.WriteLine("Ongeldige invoer. Voer een geldig getal in.");
                     continue;
                 }
-
-                if (gok == geheimGetal)
+                if (gok > 100 || gok < 1)
                 {
-                    Console.WriteLine("Gefeliciteerd je hebt het geheime getal geraden.");
-                    Console.WriteLine($"Je hebt {gokTeller} keer geraden.");
-                }
-
-                else if (gok > geheimGetal)
-                {
-                    Console.WriteLine("Het geheime getal is lager dan je gok.");
-                   
+                    Console.WriteLine("Gok mag niet hoger dan 100 worden of lager dan 1.");
                 }
                 else
                 {
-                    Console.WriteLine("Het geheime getal is hoger dan je gok.");
-                    
+                    gokTeller++;
+
+                    if (gok == geheimGetal)
+                    {
+                        Console.WriteLine("Gefeliciteerd je hebt het geheime getal geraden.");
+                        Console.WriteLine($"Je hebt {gokTeller} keer geraden.");
+                    }
+                    else if (gok > geheimGetal)
+                    {
+                        Console.WriteLine("Het geheime getal is lager dan je gok.");
+                    }
+                    else
+                    {
+                        Console.WriteLine("Het geheime getal is hoger dan je gok.");
+                    }
                 }
-               
             }
         }
     }
